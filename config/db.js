37845de7489b5b00c6fcc8d9e47b1417045341f5
devfritz <devfritzcode@gmail.com>
2024-03-09 14:37:21 -0500

    update 2024

--- config/db.js
+++ config/db.js
@@ -1,19 +1,15 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path:'./config/config.env'});
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
 
 const connectDB = async () => {
-  
-    const conn = await mongoose.connect(process.env.DATABASE_LOCAL,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology:true,
-    useFindAndModify:false
-})
- console.log(`database connected:${conn.connection.host}`);
-}
+  const conn = await mongoose.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+  console.log(`database connected:${conn.connection.host}`);
+};
 
 module.exports = connectDB;
-
-
